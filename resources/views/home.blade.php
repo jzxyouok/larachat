b1fb80c21da1928dd1497ac47f824c25a912af30
Ashraf <rootkali95@gmail.com>
2016-09-21 18:42:21 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="https://cdn.socket.io/socket.io-1.3.4.js"></script>
     <style type="text/css">
-        #messages{
+        #messages {
             border: 1px solid black;
             height: 300px;
             margin-bottom: 8px;
@@ -20,13 +20,13 @@
                     <div class="panel-body">
 
                         <div class="row">
-                            <div class="col-lg-8" >
-                                <div id="messages" ></div>
+                            <div class="col-lg-8">
+                                <div id="messages"></div>
                             </div>
-                            <div class="col-lg-8" >
+                            <div class="col-lg-8">
                                 <form action="sendmessage" method="POST">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" >
-                                    <input type="hidden" name="user" value="{{ Auth::user()->name }}" >
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <input type="hidden" name="user" value="{{ Auth::user()->name }}">
                                     <textarea class="form-control msg"></textarea>
                                     <br/>
                                     <input type="button" value="Send" class="btn btn-success send-msg">
@@ -43,25 +43,26 @@
         socket.on('message', function (data) {
             data = jQuery.parseJSON(data);
             console.log(data.user);
-            $( "#messages" ).append( "<strong>"+data.user+":</strong><p>"+data.message+"</p>" );
+            $("#messages").append("<strong>" + data.user + ":</strong><p>" + data.message + "</p>");
         });
-        $(".send-msg").click(function(e){
+
+        $(".send-msg").click(function (e) {
             e.preventDefault();
             var token = $("input[name='_token']").val();
             var user = $("input[name='user']").val();
             var msg = $(".msg").val();
-            if(msg != ''){
+            if (msg != '') {
                 $.ajax({
                     type: "POST",
                     url: '{!! URL::to("sendmessage") !!}',
                     dataType: "json",
-                    data: {'_token':token,'message':msg,'user':user},
-                    success:function(data){
+                    data: {'_token': token, 'message': msg, 'user': user},
+                    success: function (data) {
                         console.log(data);
                         $(".msg").val('');
                     }
                 });
-            }else{
+            } else {
                 alert("Please Add Message.");
             }
         })
